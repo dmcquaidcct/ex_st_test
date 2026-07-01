0a2c2217ec2b435121e57284e19ed5b0c52252f4
David McQuaid <dmcquaid@cct.ie>
2026-07-01 20:19:42 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Hello All how are you")
 st.subheader("Last Class")
 st.code('print("this is some code")')
 if st.checkbox('Balloons', False):
     st.balloons()
+    
+df=pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+st.line_chart(df)
+st.area_chart(df)
+st.bar_chart(df)
